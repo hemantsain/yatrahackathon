19519069c19a5a3e5ea2c4f0471c8ea1da9ead0c
Hemant <hemant.sain0811@gmail.com>
2016-04-02 01:37:32 +0530

    Reslove all errors

--- app/src/main/java/com/yatra/yatrahackathon/MainActivity.java
+++ app/src/main/java/com/yatra/yatrahackathon/MainActivity.java
@@ -4,13 +4,25 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.travel_result_activity);
+        setContentView(R.layout.main_activity);
+
+        RelativeLayout rlFLights = (RelativeLayout)findViewById(R.id.rl_flights);
+        RelativeLayout rlHotels = (RelativeLayout)findViewById(R.id.rl_hotels);
+        RelativeLayout rlHolidays = (RelativeLayout)findViewById(R.id.rl_holidays);
+        RelativeLayout rlTaxi = (RelativeLayout)findViewById(R.id.rl_taxi);
+
+        rlFLights.setOnClickListener(this);
+        rlHotels.setOnClickListener(this);
+        rlHolidays.setOnClickListener(this);
+        rlTaxi.setOnClickListener(this);
     }
 
     @Override
@@ -33,5 +45,19 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.rl_flights:
+                break;
+            case R.id.rl_hotels:
+                break;
+            case R.id.rl_holidays:
+                break;
+            case R.id.rl_taxi:
+                break;
+        }
     }
 }
