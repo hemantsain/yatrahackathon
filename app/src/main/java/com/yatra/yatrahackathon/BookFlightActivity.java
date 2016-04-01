8ed4992c517e4bfa6424e6ec2928b699c2b9ea96
Hemant <hemant.sain0811@gmail.com>
2016-04-02 01:37:32 +0530

    saf

---
+++ app/src/main/java/com/yatra/yatrahackathon/BookFlightActivity.java
@@ -0,0 +1,16 @@
+package com.yatra.yatrahackathon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by XVTS8308 on 02/04/2016.
+ */
+public class BookFlightActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.flights_activity);
+    }
+}
