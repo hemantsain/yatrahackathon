9877a7bd8cf560a25ea8464ad1647b1be90e74a5
Rahul-Aggrawal <raggrawa@in.ibm.com>
2016-04-02 08:38:00 +0530

    commit

--- app/src/main/java/com/yatra/yatrahackathon/AmbulancePosition.java
+++ app/src/main/java/com/yatra/yatrahackathon/AmbulancePosition.java
@@ -3,21 +3,31 @@ package com.yatra.yatrahackathon;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AmbulancePosition extends Activity implements LocationListener {
+
 	private GoogleMap map;
 	double coordinates[];
 	private String name;
+    private Button btnBookCab;
+
 	private LocationManager locationManager;
+    private MarkerOptions markerOptionsForCab;
+	private LatLng latLng;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,40 +37,71 @@ public class AmbulancePosition extends Activity implements LocationListener {
 
 		Bundle bundle = getIntent().getExtras();
 
-		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
-				.getMap();
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
 		map.setMyLocationEnabled(true);
 		map.moveCamera(CameraUpdateFactory.zoomBy(15));
 
 		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-	}
-	
-	private void setAmbLocation(double lat, double lon)
-	{
-		LatLng latLng = new LatLng(lat, lon);
-		  map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 13));
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30000, 100, this);
 
-	        map.addMarker(new MarkerOptions()
-	                .title("")
-	                .position(latLng));
-	        
+        btnBookCab = (Button) findViewById(R.id.btn_book);
+        btnBookCab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double newLatitude = latLng.latitude + 0.018;
+                double newLongitude = latLng.longitude + 0.018;
+                for (int i = 17; i >= 0; i--) {
+                    newLatitude -= 0.001;
+                    newLongitude -= 0.001;
+
+//                    if (markerOptionsForCab != null) {
+                        markerOptionsForCab.position(new LatLng(newLatitude, newLongitude));
+//                        map.addMarker(new MarkerOptions().title(name).position(new LatLng(newLatitude, newLongitude)).icon(BitmapDescriptorFactory.fromResource(R.drawable.transport)));
+//                    }
+
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    if (i == 0) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(AmbulancePosition.this).setTitle("Alert").setMessage("You cab has been reached to your door!\nTime to move on!");
+
+                        Dialog dialog = builder.create();
+                        dialog.show();
+                    }
+                }
+            }
+        });
+	}
+
+	private void setAmbLocation(double lat, double lon) {
+		LatLng latLng = new LatLng(lat, lon);
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 13));
+
+		map.addMarker(new MarkerOptions().title("").position(latLng));
+
 	}
 
 	@Override
 	public void onLocationChanged(Location location) {
-		LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+		if (latLng == null) {
+			latLng = new LatLng(location.getLatitude(), location.getLongitude());
 
-		// Showing the current location in Google Map
-		map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+			// Showing the current location in Google Map
+			map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
 
-		// Zoom in the Google Map
-		map.animateCamera(CameraUpdateFactory.zoomTo(20));
+			// Zoom in the Google Map
+			map.animateCamera(CameraUpdateFactory.zoomTo(15));
 
-		map.clear();
-		map.addMarker(new MarkerOptions()
-        .title(name)
-        .position(latLng));
+			map.clear();
+			map.addMarker(new MarkerOptions().title(name).position(latLng));
+
+            // Show cab icon
+            markerOptionsForCab = new MarkerOptions().title(name).position(new LatLng(latLng.latitude + 0.018, latLng.longitude + 0.018)).icon(BitmapDescriptorFactory.fromResource(R.drawable.transport));
+            map.addMarker(markerOptionsForCab);
+		}
 	}
 
 	@Override
--- app/src/main/java/com/yatra/yatrahackathon/notification/NotificationUtils.java
+++ app/src/main/java/com/yatra/yatrahackathon/notification/NotificationUtils.java
@@ -30,11 +30,74 @@ import java.net.URL;
  */
 public class NotificationUtils {
 
-    public static void showNotification(Context context, String message)
+    public static void showNotificationOffer(Context context, String message)
     {
 
 //        // Creates an explicit intent for an ResultActivity to receive.
         Intent resultIntent = new Intent(context, MainActivity.class);
+//        resultIntent.putExtra("news", news);
+        resultIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP );
+        // This ensures that the back button follows the recommended convention for the back key.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+//
+//        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainActivity.class);
+//
+//        // Adds the Intent that starts the Activity to the top of the stack.
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+//
+//        // Create remote view and set bigContentView.
+        RemoteViews expandedView = new RemoteViews(context.getPackageName(), R.layout.notification_for_ask_survey);
+        expandedView.setTextViewText(R.id.tvQuestion, message);
+
+
+        PendingIntent piBookNow = PendingIntent.getActivity(context, 101, new Intent(context, AmbulancePosition.class)
+                .putExtra("type", 1), PendingIntent.FLAG_UPDATE_CURRENT, null);
+
+        expandedView.setOnClickPendingIntent(R.id.btnBookNow, piBookNow);
+
+
+        PendingIntent piChoose = PendingIntent.getActivity(context, 102, new Intent(context, AmbulancePosition.class)
+                .putExtra("type", 2), PendingIntent.FLAG_UPDATE_CURRENT, null);
+
+        expandedView.setOnClickPendingIntent(R.id.btnChoose, piBookNow);
+
+
+        PendingIntent piCancel = PendingIntent.getBroadcast(context, 103, new Intent(context, NotificationBroadcast.class)
+                .putExtra("isCancel", true), PendingIntent.FLAG_UPDATE_CURRENT);
+
+//        expandedView.setOnClickPendingIntent(R.id.btnCancel, piBookNow);
+
+
+
+
+//        expandedView.setImageViewBitmap(R.id.ivMediaUrl, remote_picture);
+
+        Notification noti = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.notification_template_icon_bg)
+                .setAutoCancel(true)
+                .setContentIntent(resultPendingIntent)
+                .setContentTitle("Yatra")
+                .setContentText(message).build();
+
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
+            noti.bigContentView = expandedView;
+
+        noti.defaults |= Notification.DEFAULT_LIGHTS;
+        noti.defaults |= Notification.DEFAULT_VIBRATE;
+        noti.defaults |= Notification.DEFAULT_SOUND;
+
+        noti.flags |= Notification.FLAG_ONLY_ALERT_ONCE;
+        NotificationManager mNotificationManager = (NotificationManager)context.getSystemService(context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(2, noti);
+    }
+
+    public static void showNotification(Context context, String message)
+    {
+
+//        // Creates an explicit intent for an ResultActivity to receive.
+        Intent resultIntent = new Intent(context, AmbulancePosition.class);
 //        resultIntent.putExtra("news", news);
         resultIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP );
         // This ensures that the back button follows the recommended convention for the back key.
